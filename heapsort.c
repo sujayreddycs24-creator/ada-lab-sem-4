47e8816de90422f89d1f25ff546979056a9a4f74
unknown <sujayreddy.cs24@bmsce.ac.in>
2026-05-12 12:07:40 +0530

    heapsort codeuploading

---
+++ heapsort.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// ---------- MAX HEAPIFY ----------
+void maxHeapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        maxHeapify(arr, n, largest);
+    }
+}
+
+// ---------- MIN HEAPIFY ----------
+void minHeapify(int arr[], int n, int i) {
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] < arr[smallest])
+        smallest = left;
+    if (right < n && arr[right] < arr[smallest])
+        smallest = right;
+
+    if (smallest != i) {
+        int temp = arr[i];
+        arr[i] = arr[smallest];
+        arr[smallest] = temp;
+        minHeapify(arr, n, smallest);
+    }
+}
+
+// ---------- BOTTOM-UP HEAP SORT ----------
+void heapSortBottomUp(int arr[], int n) {
+    // Build max heap
+    for (int i = n/2 - 1; i >= 0; i--)
+        maxHeapify(arr, n, i);
+
+    // Extract elements
+    for (int i = n-1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        maxHeapify(arr, i, 0);
+    }
+}
+
+// ---------- TOP-DOWN HEAP INSERT ----------
+void insertTopDown(int heap[], int *size, int value) {
+    heap[*size] = value;
+    int i = *size;
+    (*size)++;
+
+    while (i > 0 && heap[(i-1)/2] < heap[i]) {
+        int temp = heap[i];
+        heap[i] = heap[(i-1)/2];
+        heap[(i-1)/2] = temp;
+        i = (i-1)/2;
+    }
+}
+
+// ---------- TOP-DOWN HEAP SORT ----------
+void heapSortTopDown(int arr[], int n) {
+    int heap[n];
+    int size = 0;
+
+    // Build heap incrementally
+    for (int i = 0; i < n; i++)
+        insertTopDown(heap, &size, arr[i]);
+
+    // Extract elements
+    for (int i = n-1; i >= 0; i--) {
+        arr[i] = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        maxHeapify(heap, size, 0);
+    }
+}
+
+// ---------- MIN HEAP SORT (Descending) ----------
+void heapSortDescending(int arr[], int n) {
+    // Build min heap
+    for (int i = n/2 - 1; i >= 0; i--)
+        minHeapify(arr, n, i);
+
+    // Extract elements
+    for (int i = n-1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        minHeapify(arr, i, 0);
+    }
+}
+
+// ---------- UTILITY ----------
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// ---------- MAIN ----------
+int main() {
+    int arr[] = {12, 3, 19, 6, 15, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    clock_t start, end;
+
+    // Bottom-up
+    int arr1[n];
+    for (int i = 0; i < n; i++) arr1[i] = arr[i];
+    start = clock();
+    heapSortBottomUp(arr1, n);
+    end = clock();
+    printf("Bottom-up ascending: ");
+    printArray(arr1, n);
+    printf("Time: %f seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+    // Top-down
+    int arr2[n];
+    for (int i = 0; i < n; i++) arr2[i] = arr[i];
+    start = clock();
+    heapSortTopDown(arr2, n);
+    end = clock();
+    printf("Top-down ascending: ");
+    printArray(arr2, n);
+    printf("Time: %f seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+    // Min-heap descending
+    int arr3[n];
+    for (int i = 0; i < n; i++) arr3[i] = arr[i];
+    start = clock();
+    heapSortDescending(arr3, n);
+    end = clock();
+    printf("Min-heap descending: ");
+    printArray(arr3, n);
+    printf("Time: %f seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+    return 0;
+}
